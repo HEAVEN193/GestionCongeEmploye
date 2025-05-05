13d20a98830c8de0014c45cecda60d56fa0d05dc
MATTEO-MCR <matteo.mcr@eduge.ch>
2025-05-05 16:08:53 +0200

    structure de base

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+$app->get('/', function (Request $request, Response $response, $args) {
+$response->getBody()->write('<p>Bonjour le monde</p>');
+return $response;
+});
